038735c5abdb3f9de4aa4a22f32e217a4c73ef92
jadynbb <jadynbb@gmail.com>
2026-02-25 23:48:26 -0800

    added lucide react package for svgs

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -1,52 +1,4 @@
-// interface SignUpFormProps {
-//   onToggle: () => void;
-// }
-
-// export default function SignUpForm({ onToggle }: SignUpFormProps) {
-//   return (
-//     <main className="flex flex-col items-center justify-center min-h-screen">
-//       <header className="mt-8 font-gasoek text-center text-3xl px-4 py-2 w-sm bg-rose-100 rounded-xl  text-amber-900 shadow-lg">
-//         PARTY POOPERS
-//       </header>
-
-//       <div className="flex flex-col items-center mt-8 mb-8 px-8 py-8 w-sm bg-rose-100 rounded-xl shadow-lg">
-//         <h1 className="font-gasoek text-2xl text-amber-900 text-center">
-//           SIGN UP TO START POOPING NOW!
-//         </h1>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           className="mt-8 font-rubik text-grey bg-white rounded-xl w-full px-4 py-2"
-//         />
-//         <input
-//           type="username"
-//           placeholder="Username"
-//           className="mt-8 font-rubik text-grey bg-white rounded-xl w-full px-4 py-2"
-//         />
-//         <input
-//           type="password"
-//           placeholder="Create Password"
-//           className="mt-8 font-rubik text-grey bg-white rounded-xl w-full px-4 py-2"
-//         />
-//         <input
-//           type="password"
-//           placeholder="Confirm Password"
-//           className="mt-8 font-rubik text-grey bg-white rounded-xl w-full px-4 py-2"
-//         />
-//         <button
-//         className="cursor-pointer font-rubik mt-4 px-4 py-2 bg-amber-900 rounded-xl text-white shadow-lg hover:bg-amber-800 hover:-translate-y-0.5 transition">
-//           SIGN UP
-//         </button>
-
-//          <button
-//         onClick={onToggle}
-//         className="cursor-pointer font-rubik mt-4 text-xs text-gray-400 hover:text-gray-300 hover:-translate-y-0.5 transition">
-//           ALREADY HAVE AN ACCOUNT? LOGIN HERE
-//         </button>
-//       </div>
-//     </main>
-//   );
-// }
+import { Toilet } from "lucide-react";  
 
 interface SignUpFormProps {
   onToggle: () => void;
@@ -57,8 +9,8 @@ export default function SignUpForm({ onToggle }: SignUpFormProps) {
     <div className="w-full bg-rose-100 rounded-xl shadow-lg p-8">
       <div className="flex flex-col space-y-6">
         <div className="text-center">
-          <h1 className="text-3xl font-gasoek text-amber-900">
-            SIGN UP TO START POOPING NOW!
+          <h1 className="text-2xl font-gasoek text-amber-900">
+            SIGN UP TO START POOPING NOW! <Toilet />
           </h1>
         </div>
 
@@ -105,14 +57,14 @@ export default function SignUpForm({ onToggle }: SignUpFormProps) {
         </div>
 
         {/* Submit */}
-        <button className="w-full py-2 bg-amber-900 text-white rounded-xl shadow-md hover:bg-amber-800 hover:-translate-y-0.5 transition duration-200">
+        <button className="cursor-pointer w-full py-2 bg-amber-900 text-white rounded-xl shadow-md hover:bg-amber-800 hover:-translate-y-0.5 transition duration-200">
           SIGN UP
         </button>
 
         {/* Switch to Login */}
         <button
           onClick={onToggle}
-          className="text-sm text-gray-500 hover:text-amber-900 transition"
+          className="cursor-pointer text-sm text-gray-500 hover:text-amber-900 transition"
         >
           Already have an account? <span className="underline">Login</span>
         </button>
