71835259788636a5f995ec62c95f48c2dd5ec34f
jadynbb <jadynbb@gmail.com>
2026-02-27 00:18:13 -0800

    added fully functionally nav bar and corresponding pages

---
+++ app/myprofile/page.tsx
@@ -0,0 +1,11 @@
+
+import Navbar from "../components/Navbar"
+
+export default function MyProfilePage() {
+    return (
+        <main>
+            <Navbar />
+        <p> my profile page </p>
+        </main>
+    )
+}
---
+++ app/poopers/page.tsx
@@ -0,0 +1,11 @@
+
+import Navbar from "../components/Navbar"
+
+export default function PoopersProfilePage() {
+    return (
+        <main>
+            <Navbar />
+        <p> pooper profiles page </p>
+        </main>
+    )
+}
